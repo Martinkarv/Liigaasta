6f63e727ec798eac46b55f2272a0e070b6fdfed2
Opilane TTHK <opilane@tthk.ee>
2019-09-20 18:01:34 +0300

    first commit

---
+++ Liigaasta/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liigaasta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             int sünniaasta;
+            {
+                Console.WriteLine("Sisesta palun oma sünniaasta.");
+                sünniaasta = Int32.Parse(Console.ReadLine());
+            }
+
+            if (sünniaasta % 4 == 0)
+            {
+                Console.WriteLine($"{sünniaasta} on liigaasta.");
+
+            }
+            
+            else
+            { 
+                Console.WriteLine($"{sünniaasta} ei ole liigaasta.");
+             }
+
+
+
+            Console.ReadLine();
+            */
+
+
+
+
+
+
+
+
+
+
+
+
+            //Ternary Operator
+
+            /*
+               string answer = (sünniaasta % == 0) ? "Liigaasta." : "Ei ole liigaasta.";
+
+
+            Console.WriteLine(answer);
+                Console.ReadLine();
+                */
+
+
+            Console.WriteLine("Choose your poison:Whiskey or Wine?");
+            string userChoice = Console.ReadLine();
+
+
+            string choice = userChoice == "Whiskey" ? "Here is your whiskey" : "Here is your wine.";
+            Console.WriteLine(choice);
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
